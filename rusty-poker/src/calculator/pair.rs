7e8199ce035269fa5a22a2245b235ac60ea8af40
Alexander Polyakov <apolyakov@apolyakov-spb.root.devdomain.local>
2017-10-20 15:25:43 +0300

    fixed straight and wrote tests for it

--- rusty-poker/src/calculator/pair.rs
+++ rusty-poker/src/calculator/pair.rs
@@ -3,94 +3,103 @@ use types;
 use calculator::utility;
 
 pub fn test(cards: Vec<Card>) -> Option<types::Combination> {
-  if cards.len() < 2 {
-    return None;
-  }
-
-  let hash_map = utility::get_count_hash_map(&cards[..]);
-
-  let mut current_pair: Option<types::Rank> = None;
-
-  for (&rank_value, &count) in &hash_map {
-    if count == 2 {
-      // if we have 3 or 4, then it should have been handled already
-      if let Some(current_pair_rank) = current_pair {
-        if current_pair_rank < rank_value {
-          current_pair = Some(rank_value);
-        }
-      } else {
-        current_pair = Some(rank_value);
-      }
+    if cards.len() < 2 {
+        return None;
     }
-  }
 
-  if let Some(current_pair_rank) = current_pair {
-    return Some(types::Combination::Pair(current_pair_rank));
-  }
+    let hash_map = utility::get_count_hash_map(&cards[..]);
 
-  return None;
+    let mut current_pair: Option<types::Rank> = None;
+
+    for (&rank_value, &count) in &hash_map {
+        if count == 2 {
+            // if we have 3 or 4, then it should have been handled already
+            if let Some(current_pair_rank) = current_pair {
+                if current_pair_rank < rank_value {
+                    current_pair = Some(rank_value);
+                }
+            } else {
+                current_pair = Some(rank_value);
+            }
+        }
+    }
+
+    if let Some(current_pair_rank) = current_pair {
+        return Some(types::Combination::Pair(current_pair_rank));
+    }
+
+    return None;
 }
 
 #[cfg(test)]
 mod tests {
-  use super::*;
+    use super::*;
 
-  #[test]
-  fn none_for_empty() {
-    assert_eq!(None, test(vec![]));
-  }
+    #[test]
+    fn none_for_empty() {
+        assert_eq!(None, test(vec![]));
+    }
 
-  #[test]
-  fn none_for_one_card() {
-    assert_eq!(None, test(vec![
-      Card {
-        rank: types::Rank::Two,
-        suit: types::Suit::Hearts
-      }
-    ]));
-  }
-  
-  #[test]
-  fn none_for_two_different_cards() {
-    assert_eq!(None, test(vec![
-      Card {
-        rank: types::Rank::Two,
-        suit: types::Suit::Hearts
-      },
-       Card {
-        rank: types::Rank::Three,
-        suit: types::Suit::Hearts
-      }
-    ]));
-  }   
+    #[test]
+    fn none_for_one_card() {
+        assert_eq!(
+            None,
+            test(vec![
+                Card {
+                    rank: types::Rank::Two,
+                    suit: types::Suit::Hearts,
+                },
+            ])
+        );
+    }
 
-  #[test]
-  fn chooses_highest_pair() {
-    assert_eq!(Some(types::Combination::Pair(types::Rank::Three)), test(vec![
-      Card {
-        rank: types::Rank::Two,
-        suit: types::Suit::Hearts
-      },
-       Card {
-        rank: types::Rank::Three,
-        suit: types::Suit::Hearts
-      },
-       Card {
-        rank: types::Rank::Two,
-        suit: types::Suit::Diamonds
-      },
-       Card {
-        rank: types::Rank::Ace,
-        suit: types::Suit::Diamonds
-      },
-       Card {
-        rank: types::Rank::King,
-        suit: types::Suit::Spades
-      },
-       Card {
-        rank: types::Rank::Three,
-        suit: types::Suit::Spades
-      }
-    ]));
-  }    
+    #[test]
+    fn none_for_two_different_cards() {
+        assert_eq!(
+            None,
+            test(vec![
+                Card {
+                    rank: types::Rank::Two,
+                    suit: types::Suit::Hearts,
+                },
+                Card {
+                    rank: types::Rank::Three,
+                    suit: types::Suit::Hearts,
+                },
+            ])
+        );
+    }
+
+    #[test]
+    fn chooses_highest_pair() {
+        assert_eq!(
+            Some(types::Combination::Pair(types::Rank::Three)),
+            test(vec![
+                Card {
+                    rank: types::Rank::Two,
+                    suit: types::Suit::Hearts,
+                },
+                Card {
+                    rank: types::Rank::Three,
+                    suit: types::Suit::Hearts,
+                },
+                Card {
+                    rank: types::Rank::Two,
+                    suit: types::Suit::Diamonds,
+                },
+                Card {
+                    rank: types::Rank::Ace,
+                    suit: types::Suit::Diamonds,
+                },
+                Card {
+                    rank: types::Rank::King,
+                    suit: types::Suit::Spades,
+                },
+                Card {
+                    rank: types::Rank::Three,
+                    suit: types::Suit::Spades,
+                },
+            ])
+        );
+    }
 }
--- rusty-poker/src/calculator/straight.rs
+++ rusty-poker/src/calculator/straight.rs
@@ -1,75 +1,256 @@
 use types;
-use card;
+use card::Card;
 
 use calculator::utility;
 
-pub fn test(mut cards: Vec<card::Card>) -> Option<types::Combination> {
-  if cards.len() < 5 {
-    return None;
-  }
+pub fn test(mut cards: Vec<Card>) -> Option<types::Combination> {
+    if cards.len() < 5 {
+        return None;
+    }
 
-  utility::sort_cards(&mut cards);
-
-  if test_ace_starting_straight(&cards[..]) {
-    return Some(types::Combination::Straight([
-      types::Rank::Five,
-      types::Rank::Four,
-      types::Rank::Three,
-      types::Rank::Two,
-      types::Rank::Ace,
-    ]));
-  }
-
-  for i in 0..(cards.len() - 5 + 1) {
-    let slice_to_test = &cards[i..(i + 5)];
-    if test_straight_for_slice(slice_to_test) {
-      let rank_vector = &slice_to_test
-        .into_iter()
-        .map(|&card| card.clone().rank)
+    utility::sort_cards(&mut cards);
+    let mut ranks = cards
+        .iter()
+        .map(|card| card.rank)
         .collect::<Vec<types::Rank>>();
 
-      let mut result: [types::Rank; 5] = [types::Rank::Ace; 5];
-      result.clone_from_slice(rank_vector);
-      return Some(types::Combination::Straight(result));
+    ranks.dedup();
+
+    if test_ace_finishing_straight(&ranks[..]) {
+        return Some(types::Combination::Straight(
+            [
+                types::Rank::Five,
+                types::Rank::Four,
+                types::Rank::Three,
+                types::Rank::Two,
+                types::Rank::Ace,
+            ],
+        ));
     }
-  }
-  None
+
+    for i in 0..(ranks.len() - 5 + 1) {
+        let slice_to_test = &ranks[i..(i + 5)];
+        if test_straight_for_slice(slice_to_test) {
+
+            let mut result: [types::Rank; 5] = [types::Rank::Ace; 5];
+            result.clone_from_slice(slice_to_test);
+            return Some(types::Combination::Straight(result));
+        }
+    }
+    None
 }
 
-fn test_straight_for_slice(cards: &[card::Card]) -> bool {
-  let mut prev_card = cards[0];
-  for card in cards[1..].iter() {
-    if prev_card.rank as i32 != (card.rank as i32 + 1) {
-      return false;
+fn test_straight_for_slice(ranks: &[types::Rank]) -> bool {
+    let mut prev_rank = ranks[0];
+    for rank in ranks[1..].iter() {
+        if prev_rank as i32 != (*rank as i32 + 1) {
+            return false;
+        }
+        prev_rank = *rank;
     }
-    prev_card = *card;
-  }
 
-  return true;
+    return true;
 }
 
-fn test_ace_starting_straight(cards: &[card::Card]) -> bool {
-  let ace_starting_straight_ranks = vec![
-    types::Rank::Ace,
-    types::Rank::Two,
-    types::Rank::Three,
-    types::Rank::Four,
-    types::Rank::Five,
-  ];
-  for rank_in_straight in ace_starting_straight_ranks.iter() {
-    let mut was_found = false;
+fn test_ace_finishing_straight(ranks: &[types::Rank]) -> bool {
+    let ace_finishing_straight_ranks = vec![
+        types::Rank::Ace,
+        types::Rank::Two,
+        types::Rank::Three,
+        types::Rank::Four,
+        types::Rank::Five,
+    ];
+    for rank_in_straight in ace_finishing_straight_ranks.iter() {
+        let mut was_found = false;
 
-    for card_in_hand in cards.iter() {
-      if card_in_hand.rank as i32 == *rank_in_straight as i32 {
-        was_found = true;
-        break;
-      }
+        for rank_in_hand in ranks.iter() {
+            if rank_in_hand == rank_in_straight {
+                was_found = true;
+                break;
+            }
+        }
+
+        if !was_found {
+            return false;
+        }
     }
 
-    if !was_found {
-      return false;
-    }
-  }
+    return true;
+}
 
-  return true;
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn none_for_empty() {
+        assert_eq!(None, test(vec![]));
+    }
+
+    #[test]
+    fn none_for_four_cards() {
+        assert_eq!(
+            None,
+            test(vec![
+                Card {
+                    rank: types::Rank::Four,
+                    suit: types::Suit::Spades,
+                },
+                Card {
+                    rank: types::Rank::Five,
+                    suit: types::Suit::Spades,
+                },
+                Card {
+                    rank: types::Rank::Six,
+                    suit: types::Suit::Spades,
+                },
+                Card {
+                    rank: types::Rank::Seven,
+                    suit: types::Suit::Spades,
+                },
+            ])
+        );
+    }
+
+    #[test]
+    fn returns_ace_finishing_straight() {
+        assert_eq!(
+            Some(types::Combination::Straight(
+                [
+                    types::Rank::Five,
+                    types::Rank::Four,
+                    types::Rank::Three,
+                    types::Rank::Two,
+                    types::Rank::Ace,
+                ],
+            )),
+            test(vec![
+                Card {
+                    rank: types::Rank::Three,
+                    suit: types::Suit::Spades,
+                },
+
+                Card {
+                    rank: types::Rank::Jack,
+                    suit: types::Suit::Diamonds,
+                },
+                Card {
+                    rank: types::Rank::Five,
+                    suit: types::Suit::Spades,
+                },
+                Card {
+                    rank: types::Rank::Four,
+                    suit: types::Suit::Hearts,
+                },
+                Card {
+                    rank: types::Rank::Ace,
+                    suit: types::Suit::Clubs,
+                },
+                Card {
+                    rank: types::Rank::King,
+                    suit: types::Suit::Spades,
+                },
+                Card {
+                    rank: types::Rank::Two,
+                    suit: types::Suit::Spades,
+                },
+            ])
+        );
+    }
+
+    #[test]
+    fn returns_ace_starting_straight() {
+        assert_eq!(
+            Some(types::Combination::Straight(
+                [
+                    types::Rank::Ace,
+                    types::Rank::King,
+                    types::Rank::Queen,
+                    types::Rank::Jack,
+                    types::Rank::Ten,
+                ],
+            )),
+            test(vec![
+                Card {
+                    rank: types::Rank::King,
+                    suit: types::Suit::Spades,
+                },
+
+                Card {
+                    rank: types::Rank::Jack,
+                    suit: types::Suit::Diamonds,
+                },
+                Card {
+                    rank: types::Rank::Jack,
+                    suit: types::Suit::Spades,
+                },
+                Card {
+                    rank: types::Rank::Queen,
+                    suit: types::Suit::Hearts,
+                },
+                Card {
+                    rank: types::Rank::Ace,
+                    suit: types::Suit::Clubs,
+                },
+                Card {
+                    rank: types::Rank::King,
+                    suit: types::Suit::Spades,
+                },
+                Card {
+                    rank: types::Rank::Ten,
+                    suit: types::Suit::Spades,
+                },
+            ])
+        );
+    }
+
+    #[test]
+    fn returns_queen_starting_straight() {
+        assert_eq!(
+            Some(types::Combination::Straight(
+                [
+                    types::Rank::Queen,
+                    types::Rank::Jack,
+                    types::Rank::Ten,
+                    types::Rank::Nine,
+                    types::Rank::Eight,
+                ],
+            )),
+            test(vec![
+                Card {
+                    rank: types::Rank::Eight,
+                    suit: types::Suit::Spades,
+                },
+
+                Card {
+                    rank: types::Rank::Jack,
+                    suit: types::Suit::Diamonds,
+                },
+                Card {
+                    rank: types::Rank::Jack,
+                    suit: types::Suit::Spades,
+                },
+                Card {
+                    rank: types::Rank::Queen,
+                    suit: types::Suit::Hearts,
+                },
+                Card {
+                    rank: types::Rank::Ace,
+                    suit: types::Suit::Clubs,
+                },
+                Card {
+                    rank: types::Rank::Ten,
+                    suit: types::Suit::Spades,
+                },
+                Card {
+                    rank: types::Rank::Ten,
+                    suit: types::Suit::Spades,
+                },
+                Card {
+                    rank: types::Rank::Nine,
+                    suit: types::Suit::Spades,
+                },
+            ])
+        );
+    }
 }
